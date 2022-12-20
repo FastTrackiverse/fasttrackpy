689475c74347099fd70ed880163d09754a0efe76
JoFrhwld <JoFrhwld@gmail.com>
2022-12-20 13:32:40 -0500

    smoothing function added

--- fasttrackpy/tracking.py
+++ fasttrackpy/tracking.py
@@ -1,5 +1,22 @@
 import parselmouth as pm
 import numpy as np
+import scipy.fft
+
+
+def dct_smooth(x, order = 5, out = "smooth"):
+    """
+    DCT smoother
+    """
+    coefs = scipy.fft.dct(x)
+    coef_subset = coefs[0:order]
+    smooth = scipy.fft.idct(coef_subset, n = x.shape[0])
+    if out == "smooth":
+        return(smooth)
+    elif out == "coef":
+        return(coef_subset)
+    elif out == "both":
+        return(coef_subset, smooth)
+
 
 def findformants(sound, 
                  n_formants=4,
@@ -27,3 +44,13 @@ def findformants(sound,
         ]
     )
     return(tracks)
+
+def smooth_formants(formants, 
+                    smooth_fun, 
+                    axis = 1, 
+                    **kwargs):
+    """
+    Given a formants array, smooth it according to smooth_fun
+    """
+    smoothed = np.apply_along_axis(smooth_fun, axis, formants, **kwargs)
+    return(smoothed)
