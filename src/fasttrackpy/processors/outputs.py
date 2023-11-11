56c9bfc14287038ea14da0050c635373dbe02469
JoFrhwld <JoFrhwld@gmail.com>
2023-11-11 17:29:13 -0500

    enriching output

--- src/fasttrackpy/processors/outputs.py
+++ src/fasttrackpy/processors/outputs.py
@@ -13,18 +13,24 @@ def to_dataframe(self):
     smooth_names = [
         f"F{x}_s" for x in np.arange(self.n_measured_formants)+1
     ]
+
     orig_df = pl.DataFrame(
         data = self.formants.T,
         schema=orig_names
-    )
-    orig_df = orig_df.with_columns(
-        time = pl.lit(self.time_domain)
     )
 
     smooth_df = pl.DataFrame(
         data = self.smoothed_formants.T,
         schema=smooth_names
     )
+
     out_df = pl.concat([orig_df, smooth_df], how = "horizontal")
+    
+    out_df = out_df.with_columns(
+        time = pl.lit(self.time_domain),
+        max_formant = pl.lit(self.maximum_formant),
+        n_formant = pl.lit(self.n_formants),
+        smooth_method = pl.lit(self.smoother.smooth_fun.__name__)
+    )
 
     return out_df
