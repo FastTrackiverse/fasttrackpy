6cc0514361bf212b9e2546098aef9e4619bcb9b8
JoFrhwld <JoFrhwld@gmail.com>
2023-11-13 15:29:51 -0500

    more tests

--- src/fasttrackpy/patterns/just_audio.py
+++ src/fasttrackpy/patterns/just_audio.py
@@ -1,6 +1,8 @@
 import warnings
 from pathlib import Path
 from typing import Union
+from collections.abc import Callable
+
 try:
     import magic
     no_magic = False
@@ -12,13 +14,38 @@ except:
     from sndhdr import SndHeaders
     no_magic = True
 
-def create_audio_checker(no_magic = no_magic):
+def create_audio_checker(no_magic:bool = no_magic) -> Callable:
+    """Return an audio checker, dependent on 
+       availability of libmagic.
 
-    def magic_checker(path: Union[str, Path]):
+    Args:
+        no_magic (bool): is libmagic available
+
+    Returns:
+        (Callable): A sound file checker
+    """
+
+    def magic_checker(path: str)->bool:
+        """Checks whether a file is an audio file using libmagic
+
+        Args:
+            path (str): Path to the file in question
+
+        Returns:
+            (bool): Whether or not the file is an audio file
+        """
         file_mime = magic.from_file(path, mime=True)
         return "audio" in file_mime
     
-    def sndhdr_checker(path: Union[str, Path]):
+    def sndhdr_checker(path: str)->bool:
+        """Checks whether a file is an audio file using `sndhdr`
+
+        Args:
+            path (str): Path to the file
+
+        Returns:
+            (bool): Whether or not the file is an audio file.
+        """
         hdr_info = sndhdr.what(path)
         return isinstance(hdr_info, SndHeaders)
     
@@ -26,3 +53,5 @@ def create_audio_checker(no_magic = no_magic):
         return sndhdr_checker
     
     return magic_checker
+
+is_audio = create_audio_checker(no_magic=no_magic)
--- tests/test_magic.py
+++ tests/test_magic.py
@@ -6,12 +6,12 @@ except:
     import sndhdr
 from pathlib import Path
 import pytest
-from fasttrackpy.patterns.just_audio import create_audio_checker
+from fasttrackpy.patterns.just_audio import create_audio_checker,\
+                                            is_audio
+
 class TestMagicImport:
     @pytest.mark.skipif(no_magic, reason = "libmagic unavailable")
     def test_magic_import(self):
-        m = magic.Magic()
-        assert m
         audio_path = Path("tests", "test_data", "ay.wav")
         x = magic.from_file(str(audio_path), mime=True)
         assert x
@@ -20,3 +20,10 @@ class TestMagicImport:
     def test_conditional_checker(self):
         checker = create_audio_checker(no_magic=no_magic)
         assert checker
+        audio_path = Path("tests", "test_data", "ay.wav")
+        is_audio = checker(str(audio_path))
+        assert is_audio
+
+    def test_universal_checker(self):
+        audio_path = Path("tests", "test_data", "ay.wav")
+        assert is_audio(str(audio_path))
