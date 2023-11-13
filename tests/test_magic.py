9aaf571a5b956a987cef14ced1ce1338448863a3
JoFrhwld <JoFrhwld@gmail.com>
2023-11-13 15:17:31 -0500

    we're getting there

--- tests/test_magic.py
+++ tests/test_magic.py
@@ -13,7 +13,7 @@ class TestMagicImport:
         m = magic.Magic()
         assert m
         audio_path = Path("tests", "test_data", "ay.wav")
-        x = magic.from_file(audio_path, mime=True)
+        x = magic.from_file(str(audio_path), mime=True)
         assert x
         assert "audio" in x
 
