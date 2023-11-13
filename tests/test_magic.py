915c629eaf2501d1abe296784493a0025e10b75e
JoFrhwld <JoFrhwld@gmail.com>
2023-11-13 14:38:19 -0500

    conditional magic import

--- tests/test_magic.py
+++ tests/test_magic.py
@@ -1,8 +1,13 @@
-import magic
+try:
+    import magic
+except:
+    no_magic = True
+    import sndhdr
 from pathlib import Path
+import pytest
 
 class TestMagicImport:
-
+    @pytest.mark.skipif(no_magic, reason = "libmagic unavailable")
     def test_magic_import(self):
         m = magic.Magic()
         assert m
