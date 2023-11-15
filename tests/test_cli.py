163334a07268a591c4a4ed63ef59e1bdcb135421
JoFrhwld <JoFrhwld@gmail.com>
2023-11-15 13:18:17 -0500

    cli tests

---
+++ tests/test_cli.py
@@ -0,0 +1,35 @@
+from fasttrackpy.cli import fasttrack
+from pathlib import Path
+from click.testing import CliRunner
+import pytest
+
+
+class TestCLI:
+    sound_path = Path("tests", "test_data", "ay.wav")
+    def test_file_usage(self):
+        runner = CliRunner()
+        runner.invoke(
+            fasttrack,
+            f"--file {str(self.sound_path)} --output {str(self.sound_path.with_suffix('.csv'))}"
+        )
+
+        assert self.sound_path.with_suffix('.csv').is_file()
+        self.sound_path.with_suffix('.csv').unlink()
+
+
+    def test_dir_usage(self):
+        out_dir = self.sound_path.parent.joinpath("output")
+        if not out_dir.is_dir():
+            out_dir.mkdir()
+
+        runner = CliRunner()
+        runner.invoke(
+            fasttrack,
+            f"--dir {str(self.sound_path.parent)} --dest {str(out_dir)}"
+        )
+        
+        out_files = list(out_dir.glob("*"))
+        assert all([x.is_file() for x in out_files])
+
+        [x.unlink() for x in out_files]
+        out_dir.rmdir()
