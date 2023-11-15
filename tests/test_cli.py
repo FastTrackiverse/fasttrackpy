38d30f6b92ff541da6aa2ff974bf45c9bcb68fe0
JoFrhwld <JoFrhwld@gmail.com>
2023-11-15 13:33:51 -0500

    trying to fix test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,15 +7,18 @@ import pytest
 class TestCLI:
     sound_path = Path("tests", "test_data", "ay.wav")
     def test_file_usage(self):
-        output_file = self.sound_path.with_suffix(".csv")
+        out_dir = self.sound_path.parent.joinpath("output")
+        if not out_dir.is_dir():
+            out_dir.mkdir()
+
         runner = CliRunner()
         runner.invoke(
             fasttrack,
-            f"--file {str(self.sound_path)} --output {str(output_file)}"
+            f"--file {str(self.sound_path)} --dest {str(out_dir)}"
         )
-
-        assert output_file.is_file()
-        output_file.unlink()
+        out_files = list(out_dir.glob("*"))
+        [x.unlink() for x in out_files]
+        out_dir.rmdir()
 
 
     def test_dir_usage(self):
