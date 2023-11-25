da630689f56d343fdce46187704f267ea7a931ec
JoFrhwld <JoFrhwld@gmail.com>
2023-11-25 13:12:44 -0500

    testing list write

--- tests/test_patterns/test_audio_textgrid.py
+++ tests/test_patterns/test_audio_textgrid.py
@@ -5,6 +5,7 @@ from fasttrackpy.tracks import Track,\
                                Loss, \
                                Agg
 from fasttrackpy.patterns.audio_textgrid import process_audio_textgrid
+from fasttrackpy.processors.outputs import write_data
 from aligned_textgrid import SequenceInterval
 
 import parselmouth as pm
@@ -26,6 +27,10 @@ class TestAudioTG:
         assert candidates[0].winner.group
         assert isinstance(candidates[0].interval, SequenceInterval)
 
+        df = candidates[0].to_df()
+        assert "group" in df.columns
+        assert "id" in df.columns
+
     def test_audio_tg2(self)    :
         candidates = process_audio_textgrid(
             audio_path=AUDIO_PATH,
@@ -38,3 +43,9 @@ class TestAudioTG:
         assert isinstance(candidates[0], CandidateTracks)
         assert candidates[0].winner.group
         assert all(["AY" in x.winner.label for x in candidates])
+        out_file = Path("tests", "test_data", "output.csv")
+        write_data(candidates, file = out_file)
+
+        assert out_file.exists()
+
+        out_file.unlink()
