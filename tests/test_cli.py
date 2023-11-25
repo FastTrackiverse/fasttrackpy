ccd3ecea8010220e504194f91d74b264b1d71e9d
JoFrhwld <JoFrhwld@gmail.com>
2023-11-25 13:20:02 -0500

    audio-tg test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,6 +6,8 @@ import pytest
 
 class TestCLI:
     sound_path = Path("tests", "test_data", "ay.wav")
+    audio_path = Path("tests", "test_data", "corpus", "josef-fruehwald_speaker.wav")
+    tg_path = Path("tests", "test_data", "corpus", "josef-fruehwald_speaker.TextGrid")
     def test_file_usage(self):
         out_dir = self.sound_path.parent.joinpath("output")
         if not out_dir.is_dir():
@@ -14,7 +16,7 @@ class TestCLI:
         runner = CliRunner()
         runner.invoke(
             fasttrack,
-            f"--file {str(self.sound_path)} --dest {str(out_dir)}"
+            f"audio --file {str(self.sound_path)} --dest {str(out_dir)}"
         )
         out_files = list(out_dir.glob("*"))
         assert all([x.is_file() for x in out_files])
@@ -30,7 +32,7 @@ class TestCLI:
         runner = CliRunner()
         runner.invoke(
             fasttrack,
-            f"--dir {str(self.sound_path.parent)} --dest {str(out_dir)}"
+            f"audio --dir {str(self.sound_path.parent)} --dest {str(out_dir)}"
         )
         
         out_files = list(out_dir.glob("*"))
@@ -38,3 +40,16 @@ class TestCLI:
 
         [x.unlink() for x in out_files]
         out_dir.rmdir()
+
+    def test_audio_tg(self):
+        out_file = Path("tests", "test_data", "vowels.csv")
+        runner = CliRunner()
+        runner.invoke(
+            fasttrack,
+            f"audio-textgrid --audio {str(self.audio_path)} --textgrid {str(self.tg_path)} --target-tier Phone --target-labels AY --output {str(out_file)}"
+        )
+
+        assert out_file.exists()
+
+        out_file.unlink()
+
