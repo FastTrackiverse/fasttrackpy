38465dd18e3b58e7ed40280a62a7540fb742e3bf
JoFrhwld <JoFrhwld@gmail.com>
2024-05-21 11:58:35 -0400

    docstrings for filter nones

--- src/fasttrackpy/utils/safely.py
+++ src/fasttrackpy/utils/safely.py
@@ -10,6 +10,7 @@ def safely(
     A decorator for more graceful failing. 
     If the decorated function raises an exception, 
     it will return `None`. 
+    
 
     Args:
         message (str, optional): 
@@ -29,7 +30,40 @@ def safely(
 def filter_nones(
     filterer: Sequence,
     to_filter: list[Sequence]
-):
+)->list[Sequence]:
+    """
+    Filter lists based on the presence of None values.
+
+    #### Usage: 
+    ```{python}
+    # on a single list
+    from fasttrackpy.utils.safely import filter_nones
+    a = [1, 2, None, 6]
+
+    # value unpacking
+    a, = filter_nones(a, [a])
+    print(a)
+    ```
+
+    ```{python}
+    from fasttrackpy.utils.safely import filter_nones
+    a = [1, 2, None, 6]
+    b = ["a", "b", "c", "d"]
+
+    a,b = filter_nones(a, [a,b])
+    print(a)
+    print(b)
+    ```
+
+    Args:
+        filterer (Sequence):
+            The filterer list that may contain `None` values
+        to_filter (list[Sequence]):
+            A list of lists to filter.
+
+    Returns:
+        list[Sequence]: _description_
+    """
     nones = [i for i,x in enumerate(filterer) if x is None]
     filtered = [
         [x for i,x in enumerate(one_list) if not i in nones]
